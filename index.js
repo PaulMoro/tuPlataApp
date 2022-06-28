1e34f6e7ef019cb4c4c6afd33b0680a71b400eb4
Paul Montoya <paulmontoya15@gmail.com>
2022-06-27 23:58:11 -0500

    update to disign

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let blockElements = {
-  imageLink: 'https://tuplatapp.com/wp-content/uploads/2021/11/FaviconTPA-02.png',
+  imageBackgound: './back.png',
+  iconLike: './like.svg',
+  iconInfo: './info.svg',
   initTime: '3-12 meses',
+  titleHeader: 'Cómodas cuotas que te hacen la vida más fácil',
   valueMax: 2000000,
   valueMin: 300000,
   valueStep: 50000,
@@ -21,7 +24,7 @@ let blockElements = {
   taxes: 0.19,
   titleGMF: 'GMF',
   valueGMF: 0.004,
-  titleTotal: 'Total',
+  titleTotal: 'Total a pagar',
   titleContinue: 'Continuar',
   textCondition: '*Estos datos no son reales, hacemos un estudio de credito previo',
   linkContinue: '/register',
@@ -149,16 +152,23 @@ const initCalculator = (objectInit) => {
   elemetDOM.innerHTML = `
   <style>
   :root{
-    --principal-color: #0054a4;
+    --principal-color: #0B56A2;
     --secondary-color: #ddd;
     --border: #ebf2f5;
+    --border-color: #113899;
+    --background-input: #E8F0F7;
+    --text-blue: #0B56A2;
+    --text-blue-black: #233956;
+    --text-gray: #555656;
+    --text-gray-blue: #BCCAE2;
   }
   #tu-plata-app{
   
   }
   #tu-plata-app .calculator{
-    border: solid var(--secondary-color) 3px;
+    border: solid var(--border-color) 4px;
     border-radius: 10px;
+    padding: 0 0.6rem 0;
   }
   
   #tu-plata-app .calculator__container{
@@ -182,9 +192,9 @@ const initCalculator = (objectInit) => {
     border: 0;
     border-radius: 500px;
     width: 100%;
-    height: 1rem;
+    height: 0.72rem;
     outline: 0;
-    background-color: var(--secondary-color);
+    background-color: var(--background-input);
     -webkit-appearance: none;
     appearance: none;
   }
@@ -207,27 +217,20 @@ const initCalculator = (objectInit) => {
     appearance: none;
   }
   
+  #tu-plata-app .calculator-image{
+    display: none;
+    background-image: url(${objectInit.imageBackgound});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 110%;
+  }
+  
   #tu-plata-app .calculator-header{
     padding-top: 1rem;
-    padding-bottom: 0.5rem;
+    padding-bottom: 2rem;
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-  
-  #tu-plata-app .calculator-header .calculator-header__logo{
-    width: 30%;
-    max-width: 5rem;
-    max-height: 9rem;
-  }
-  
-  #tu-plata-app .calculator-header .calculator-header__logo img{
-      max-width: 100%;
-      max-height: 4rem;
-      border: none;
-      border-radius: 0;
-      -webkit-box-shadow: none;
-      box-shadow: none;
   }
   
   #tu-plata-app .calculator-header .calculator-header__data{
@@ -241,7 +244,7 @@ const initCalculator = (objectInit) => {
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 0 0 0.5rem 0;
+    padding: 0 0 1rem 0;
     flex-wrap: wrap;
   }
   
@@ -249,26 +252,48 @@ const initCalculator = (objectInit) => {
     border-radius: 1.5em;
     padding: 0.5rem 0.875rem 0.25rem;
     text-align: center;
-    font-size: 0.9rem;
+    font-size: 1.4rem;
     color: white;
     background-color: var(--principal-color);
   }
   
+  #tu-plata-app .calculator-header .calculator-header__data .header__value span{
+    color: var(--text-gray-blue);
+  }
+  
   #tu-plata-app .calculator-header .calculator-header__data .calculator-header__time{
     margin: 0;
-    padding: 0 0 0 0.5rem;
+    padding: 0 0 0 1rem;
+    font-size: 1.4rem;
     text-align: center;
+    color: var(--text-blue);
   }
   
   #tu-plata-app .calculator-header .calculator-header__sentence{
     margin: 0;
+    padding: 0 0 0 2rem;
+    position: relative;
     font-size: 0.8rem;
     text-align: center;
+    color: var(--text-gray);
+  }
+  
+  #tu-plata-app .calculator-header .calculator-header__sentence::before{
+    content: '';
+    width: 2rem;
+    height: 1.9rem;
+    position: absolute;
+    left: -0.6rem;
+    top: 0;
+    transform: translate(50%, -5px);
+    background-image: url(${objectInit.iconLike});
+    background-repeat: no-repeat;
   }
   
   #tu-plata-app .calculator-body{
-    border-bottom: 1px solid var(--border);
-    border-top: 2px solid var(--border);
+    border-bottom: 3px solid var(--border);
+    border-top: 3px solid var(--border);
+    padding: 0 0 1.8rem;
   }
   
   #tu-plata-app .calculator-body__method{
@@ -280,17 +305,23 @@ const initCalculator = (objectInit) => {
   }
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__features-title{
+    font-size: 1.6rem; /**/
+    color: var(--text-blue-black);
   }
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__features-title--value{
     padding: 0 0 0 0;
+    max-width: 10rem; /**/
+    font-size: 1.4rem; /**/
+    color: var(--text-blue-black);
   }
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__features-title--value#value-to-paid{
+    max-width: 6.5rem;/**/
   }
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__features-title--value#month{
-   width: 1.5rem;
+   width: 1.75rem; /**/
    text-align: center !important;
   }
   
@@ -312,11 +343,20 @@ const initCalculator = (objectInit) => {
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__value--share span{
     font-size: 0.825rem;
+    color: var(--text-blue-black);
   }
   
   #tu-plata-app .calculator-body .calculator-body__method div.calculator-body__value--share input[type='text']{
-    padding-top: 0;
+    padding-top: 0.2rem;
+    padding-bottom: 0;
     text-align: center !important;
+    color: var(--text-blue-black);
+  }
+  
+  #tu-plata-app .calculator-body .calculator-body__method div.calculator-body__value--money input[type='text']{
+    padding-top: 0.2rem;
+    padding-bottom: 1rem;
+    color: var(--text-blue-black);
   }
   
   #tu-plata-app .calculator-body .calculator-body__method .calculator-body__value p,
@@ -334,12 +374,20 @@ const initCalculator = (objectInit) => {
   
   #tu-plata-app .calculator-footer .calculator-footer__title{
     padding: 0 0 0.875rem;
+    font-size: 1.4rem;
     text-align: center;
+    color: var(--text-blue);
   }
   
   #tu-plata-app .calculator-footer .calculator-footer__amount{
     display: flex;
     flex-direction: column;
+    color: var(--text-gray);
+  }
+  
+  #tu-plata-app .calculator-footer .calculator-footer__amount input[type="text"]{
+    text-align: end;
+    color: var(--text-gray);
   }
   
   #tu-plata-app .calculator-footer .calculator-footer__amount > ul{
@@ -354,6 +402,20 @@ const initCalculator = (objectInit) => {
   }
   
   #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--title{
+    padding: 0 0 0 1rem;
+    position: relative;
+  }
+  
+  #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--title::before{
+    content: '';
+    width: 13px;
+    height: 13px;
+    position: absolute;
+    left: -0.6rem;
+    top: 0;
+    transform: translate(50%, 1px);
+    background-image: url(${objectInit.iconInfo});
+    background-repeat: no-repeat;
   }
   
   #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--total{
@@ -367,32 +429,52 @@ const initCalculator = (objectInit) => {
   
   #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--total p{
     width: 100%;
+    padding: 0 0 0 1rem;
+    position: relative;
     display: flex;
     justify-content: space-between;
   }
   
+  #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--total p::before{
+    content: '';
+    width: 13px;
+    height: 13px;
+    position: absolute;
+    left: -0.6rem;
+    top: 0;
+    transform: translate(50%, 1px);
+    background-image: url(${objectInit.iconInfo});
+    background-repeat: no-repeat;
+  }
+  
   #tu-plata-app .calculator-footer .calculator-footer__continue{
+    margin: 1rem 0 0;
     display: flex;
     justify-content: center;
   }
   
   #tu-plata-app .calculator-footer .calculator-footer__continue a{
+    border-radius: 30px;
+    padding: 0.5rem;
+    min-width: 50%;
     display: block;
-    padding: 1rem 0.5rem;
-    border-radius: 5px;
     background-color: var(--principal-color);
-    width: fit-content;
-    color: var(--secondary-color);
+    color: #FFF;
     text-decoration: none;
+    text-align: center;
+    font-size: 1.6rem;
   }
   @media (min-width: 680px) {
     #tu-plata-app .calculator{
+      padding: 0 1.6rem 0;
       display: grid;
-      grid-template-columns: 3fr 8fr;
-      grid-template-rows: 1fr 1fr;
+      grid-template-columns: auto;
+      grid-template-rows: auto auto auto;
     }
   
-    #test .calculator-header{
+    #tu-plata-app .calculator-header{
+      border-bottom: 3px solid var(--border);
+      padding-bottom: 4rem;
       grid-column: 1 / 2;
       grid-row: 1 / 2;
     }
@@ -402,31 +484,52 @@ const initCalculator = (objectInit) => {
       display: flex;
       flex-direction: column;
       justify-content: center;
-      border-left: 2px solid var(--secondary-color);
-      grid-column: 2 / 3;
-      grid-row: 1 / 2;
+      grid-column: 1 / 2;
+      grid-row: 2 / 3;
     }
   
     #tu-plata-app .calculator-footer{
-      border-top: 1px solid var(--secondary-color);
-      grid-column: 1 / 3;
-      grid-row: 2 / 3;
+      border-top: 3px solid var(--border);
+      grid-column: 1 / 2;
+      grid-row: 3 / 4;
     }
   
     #tu-plata-app .calculator-footer .calculator-footer__amount{
       display: grid;
-      grid-template-columns: auto auto;
+      grid-template-columns: 50% 50%;
       grid-template-rows: auto auto;
     }
   
     #tu-plata-app .calculator-footer .calculator-footer__amount > ul:first-of-type{
       grid-column: 1/2;
       grid-row: 1/3;
+      position: relative;
     }
   
+    #tu-plata-app .calculator-footer .calculator-footer__amount > ul:first-of-type input[type="text"]{
+      padding: 0 2rem 0 0;
+    }
+    
     #tu-plata-app .calculator-footer .calculator-footer__amount > ul:last-of-type{
       grid-column: 2/3;
       grid-row: 1/2;
+      padding: 0 0 0 2rem;
+      position: relative;
+    }
+  
+    #tu-plata-app .calculator-footer .calculator-footer__amount > ul:last-of-type::after{
+      content: '';
+      width: 3px;
+      height: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+      transform: translate(-50%, 1px);
+      background-color: var(--border);
+    }
+  
+    #tu-plata-app .calculator-footer .calculator-footer__amount .calculator-footer__amount--total{
+      padding: 0 0 0 2rem;
     }
   
     #tu-plata-app .calculator-footer .calculator-footer__amount > div{
@@ -442,28 +545,71 @@ const initCalculator = (objectInit) => {
     #tu-plata-app {
       padding: 0 3rem;
     }
+  
+    #tu-plata-app .calculator{
+      padding: 0;
+      display: grid;
+      grid-template-columns: 200px 3fr 8fr;
+      grid-template-rows: auto auto auto;
+    }
+  
+    #tu-plata-app .calculator-image{
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+      display: block;
+      grid-column: 1 / 2;
+      grid-row: 1 / 4;
+    }
+  
+    #tu-plata-app .calculator-header{
+      grid-column: 2 / 4;
+      grid-row: 1 / 2;
+      border: none;
+    }
+  
+    #tu-plata-app .calculator-body{
+      grid-column: 2 / 4;
+      grid-row: 2 / 3;
+      flex-direction: row;
+      padding: 0 1.6rem 1.8rem;
+    }
+  
+    #tu-plata-app .calculator-body__method.money{
+      padding: 0 2.6rem 0 0;
+      justify-content: space-between;
+    }
+  
+    #tu-plata-app .calculator-body__method.share{
+      border-left: 3px solid var(--background-input);
+      padding: 0 0 0 2.6rem;
+    }
+    
+    #tu-plata-app .calculator-footer{
+      margin: 0 1.6rem 0;
+      border-top: 3px solid var(--border);
+      grid-column: 2 / 4;
+      grid-row: 3 / 4;
+    }
   }
   @media (min-width: 1200px) {
-    #tu-plata-app {
+  /*   #tu-plata-app {
       padding: 0;
-    }
+    } */
   }
   </style>
      <div class="calculator">
+        <div class="calculator-image"></div>
         <div class="calculator-header calculator__container">
-            <div class="calculator-header__logo">
-                <img src="${objectInit.imageLink}" alt="">
-            </div>
             <div class="calculator-header__data">
                 <div>
                     <div class="header__value">
-                      <sup>hasta</sup>$ ${formatNumber(objectInit.valueMax)}
+                      <span>hasta</span> $${formatNumber(objectInit.valueMax)}
                     </div>
                     <p class="calculator-header__time">
                       ${objectInit.initTime}
                     </p>
                 </div>
-                <p class="calculator-header__sentence">Crédito en cómodas cuotas mensuales</p>
+                <p class="calculator-header__sentence"> ${objectInit.titleHeader}</p>
             </div>
         </div>
         
@@ -478,13 +624,15 @@ const initCalculator = (objectInit) => {
                     <label for="value-request" hidden>Valor que solicita</label>
                     <input type="text" id="value-request" name="value-request" class="calculator-body__features-title--value" id='value-request' value="$ ${formatNumber(objectInit.valueInit)}" readonly>
                 </p>
-                <div class="calculator-body__input">
-                    <label for="value-modificator" hidden>Valor a solicitar</label>
-                    <input type="range" class="input__bar" id="value-modifier-money" name="value-modificator" min="${objectInit.valueMin}" max="${objectInit.valueMax}" value="${objectInit.valueInit}" step="${objectInit.valueStep}">
-                </div>
-                <div class="calculator-body__value">
-                    <input type="text" class="calculator-body__value--left" value="$ ${formatNumber(objectInit.valueMin)}">
-                    <input type="text" class="calculator-body__value--rigth"  value="$ ${formatNumber(objectInit.valueMax)}">
+                <div style="width: 100%;">
+                  <div class="calculator-body__input">
+                      <label for="value-modificator" hidden>Valor a solicitar</label>
+                      <input type="range" class="input__bar" id="value-modifier-money" name="value-modificator" min="${objectInit.valueMin}" max="${objectInit.valueMax}" value="${objectInit.valueInit}" step="${objectInit.valueStep}">
+                  </div>
+                  <div class="calculator-body__value calculator-body__value--money">
+                      <input type="text" class="calculator-body__value--left" value="$ ${formatNumber(objectInit.valueMin)}">
+                      <input type="text" class="calculator-body__value--rigth"  value="$ ${formatNumber(objectInit.valueMax)}">
+                  </div>
                 </div>
             </div>
             
@@ -492,10 +640,10 @@ const initCalculator = (objectInit) => {
             
             <div class="calculator-body__method share">
                 <p class="calculator-body__features-title">
-                    Cuota mensual de
+                    Cuota mensual
                     <label for="value-to-paid" hidden>Valor de cuota</label>
                     <input type="text" id="value-to-paid" name="value-to-paid" class="calculator-body__features-title--value" value="$ ${ formatNumber( seachIntere(objectInit.valueInit, objectInit.interesForMounth, objectInit.timeInit))}" readonly>
-                     </br> por
+                     por
                     <label for="month" hidden>Cantidad de meses</label>
                     <input 
                       type="text" 
@@ -511,12 +659,12 @@ const initCalculator = (objectInit) => {
                     <input type="range" class="input__bar" id="value-modifier-share" name="value-modifier-share" min="${objectInit.timeMin}" max="${objectInit.timeMax}" value="${objectInit.timeInit}" step="1">
                 </div>
                 <div class="calculator-body__value calculator-body__value--share">
-                    <span>por 3 meses</span>
-                    <span>por 12 meses</span>
-                    <label for="paid-first" hidden>Cantidad por tres meses</label>
-                    <input type="text" id="paid-first" name="paid-first" class="calculator-body__value--rigth" value="$ ${ formatNumber( seachIntere(objectInit.valueInit, objectInit.interesForMounth, objectInit.timeMin))}" readonly>
-                    <label for="paid-last" hidden>Cantidad por doce meses</label>
-                    <input type="text" id="paid-last" name="paid-last" class="calculator-body__value--left" value="$ ${formatNumber( seachIntere(objectInit.valueInit, objectInit.interesForMounth, objectInit.timeMax))}" readonly>
+                  <label for="paid-first" hidden>Cantidad por tres meses</label>
+                  <input type="text" id="paid-first" name="paid-first" class="calculator-body__value--rigth" value="$ ${ formatNumber( seachIntere(objectInit.valueInit, objectInit.interesForMounth, objectInit.timeMin))}" readonly>
+                  <label for="paid-last" hidden>Cantidad por doce meses</label>
+                  <input type="text" id="paid-last" name="paid-last" class="calculator-body__value--left" value="$ ${formatNumber( seachIntere(objectInit.valueInit, objectInit.interesForMounth, objectInit.timeMax))}" readonly>
+                  <span>por 3 meses</span>
+                  <span>por 12 meses</span>
                 </div>
             </div>
         </div>
@@ -621,7 +769,7 @@ const initCalculator = (objectInit) => {
             </div>
         </div>
     </div>
-    <span style="font-size: 0.8rem;">${objectInit.textCondition}</span>`
+    <span style="padding: 0 0 0 2rem;font-size: 0.8rem;">${objectInit.textCondition}</span>`
   changeToRequest()
   changeMonth(objectInit.valueInit)
 }
